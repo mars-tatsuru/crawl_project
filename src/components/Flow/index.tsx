c03564edd2766110ad06b51dd9a304c8f2f44b05
tatsuru koike <t-koike@marsflag.com>
2024-02-12 20:38:46 +0900

    feat: learn react flow

--- src/components/Flow/index.tsx
+++ src/components/Flow/index.tsx
@@ -1,195 +1,56 @@
 "use client";
 
-import { useCallback, useMemo, useEffect } from "react";
+import React, { useCallback } from "react";
 import ReactFlow, {
-  ReactFlowProvider,
-  Panel,
-  Node,
+  MiniMap,
+  Controls,
+  Background,
   useNodesState,
   useEdgesState,
   addEdge,
-  Connection,
-  Edge,
-  ConnectionLineType,
-  Position,
+  BackgroundVariant,
+  Panel,
 } from "reactflow";
-import dagre from "dagre";
 
-import CustomNode from "@/components/Flow/CustomNode";
-import styles from "@/styles/Flow.module.scss";
-import json from "../../backend/storage/key_value_stores/default/page_data.json";
-import CustomEdge from "@/components/Flow/CustomEdge";
+import "reactflow/dist/style.css";
 
-/****************************
- *style for the box
- *****************************/
-const nodeTypes = {
-  custom: CustomNode,
-};
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
-const initialNodes: Node[] = [];
-
-const edgeTypes = {
-  "custom-edge": CustomEdge,
-};
-
-/****************************
- * type of Json data
- ****************************/
-type Data = {
-  url: string;
-  title: string;
-  level: number;
-};
-
-type LevelAndUrl<T extends Data> = {
-  level: T["level"];
-  url: T["url"];
-};
-
-/******************************************
- * processing json data
- ******************************************/
-const jsonData: Data[] = json;
-
-// sorting the data by level
-jsonData.sort((a, b) => a.level - b.level);
-
-const levelAndUrlArr: LevelAndUrl<Data>[] = jsonData.map((data) => {
-  return { level: data.level, url: data.url };
-});
-
-const levelArrProcessed: string[] = [];
-/********************************************************
- * style for the edges(lines)
- ********************************************************/
-const initialEdges: Edge[] = [];
-
-// const initialValue = 0;
-const initialValue = {
-  level: 0,
-  url: "",
-};
-let count = 1;
-let sideFlagArr: boolean[] = [];
-
-// almost success. but not convinced.
-levelAndUrlArr.reduce((acc, curr, index, arr) => {
-  if (index === 0) {
-    // not empty string
-    initialEdges.push({
-      id: `${index}`,
-      source: "0",
-      target: "0",
-      type: "custom-edge",
-    });
-
-    // first level url is stored as is
-    levelArrProcessed.push(`${curr.level}`);
-
-    sideFlagArr.push(true);
-
-    return curr;
-  }
-
-  // if the level is the same as the previous level
-  if (acc.level === curr.level) {
-    // if the level is the same as the previous level and the level before the previous level
-    if (arr[index - 2] && curr.level !== arr[index - 2].level) {
-      count = 1;
-    }
-
-    if (parseInt(String(curr.level)) === parseInt(String(acc.level))) {
-      sideFlagArr.push(true);
-
-      initialEdges.push({
-        id: `${acc.level}->${curr.level}${count}`,
-        source: `${acc.level}`,
-        target: `${curr.level}${count}`,
-        type: "custom-edge",
-      });
-
-      levelArrProcessed.push(`${curr.level}${count}`);
-    } else {
-      sideFlagArr.push(false);
-
-      initialEdges.push({
-        id: `${acc.level}->${curr.level}${count}`,
-        source: `${acc.level}`,
-        target: `${curr.level}${count}`,
-        type: "custom-edge",
-      });
-
-      levelArrProcessed.push(`${curr.level}${count}`);
-    }
-
-    count++;
-
-    return curr;
-  } else {
-    initialEdges.push({
-      id: `${acc.level}->${curr.level}`,
-      source: `${acc.level}`,
-      target: `${curr.level}`,
-      type: "custom-edge",
-    });
-    levelArrProcessed.push(`${curr.level}`);
-    return curr;
-  }
-}, initialValue);
-
-/******************************************
- *  create a node for each url
- ******************************************/
-jsonData.forEach((data, index) => {
-  const newNode = {
-    id: `${levelArrProcessed[index]}`,
-    data: {
-      id: `${index + 1}`,
-      title: data.title,
-      url: data.url,
-      level: data.level,
-      sideFlag: sideFlagArr[index],
-    },
-    position: {
-      x: 0,
-      y: 0,
-    },
-    targetPosition: Position.Left,
-    sourcePosition: Position.Right,
-    type: "custom",
-    className: styles.customNode,
-  };
-  initialNodes.push(newNode);
-});
-
-/****************************
- *Layout
- *****************************/
-
-function Flow() {
+export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect = useCallback(
-    (connection: Connection) => {
-      const edge = { ...connection, type: "custom-edge" };
-      setEdges((eds) => addEdge(edge, eds));
-    },
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   );
 
+  const backgroundVariant = BackgroundVariant.Cross;
+
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      edgeTypes={edgeTypes}
-      fitView
-    />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Panel position="top-left">top-left</Panel>
+        <Panel position="top-center">top-center</Panel>
+        <Panel position="top-right">top-right</Panel>
+        <Panel position="bottom-left">bottom-left</Panel>
+        <Panel position="bottom-center">bottom-center</Panel>
+        <Panel position="bottom-right">bottom-right</Panel>
+        <Controls />
+        <MiniMap />
+        <Background variant={backgroundVariant} gap={12} size={1} />
+      </ReactFlow>
+    </div>
   );
 }
-
-export default Flow;
