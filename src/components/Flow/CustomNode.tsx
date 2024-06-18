37cc1544d52da71ae72bb30624780fa689227fee
tatsuru koike <t-koike@marsflag.com>
2024-06-18 18:02:13 +0900

    add fold up functions :WIP

--- src/components/Flow/CustomNode.tsx
+++ src/components/Flow/CustomNode.tsx
@@ -1,8 +1,62 @@
 import { memo, FC, CSSProperties } from "react";
-import { Handle, Position, NodeProps, NodeResizer } from "reactflow";
+import { Handle, Position, NodeProps, useNodeId, NodeResizer } from "reactflow";
 import Image from "next/image";
 
 import styles from "@/styles/Custom.module.scss";
+
+const foldUpCard = (e: any) => {
+  e.stopPropagation();
+  e.preventDefault();
+
+  // closest method to find the closest ancestor with the specific class
+  const dom = e.target.closest(".react-flow__node-custom");
+
+  // If no such element is found, exit the function
+  if (!dom) {
+    return;
+  }
+
+  // Assuming the data-id is stored as a data attribute
+  const nodeId = dom.getAttribute("data-id");
+
+  // Get all elements with the specific class
+  const allNodes = document.querySelectorAll(".react-flow__node-custom");
+  let nodeArray: any[] = [];
+  let nodePosition: any[] = [];
+
+  allNodes.forEach((node) => {
+    // Get the data-id attribute of each node
+    const currentId = node.getAttribute("data-id");
+
+    if (currentId?.includes(nodeId)) {
+      // Toggle the 'folded' class for the clicked node
+      nodeArray.push(node);
+      // nodePosition.push(node.getBoundingClientRect());
+    } else {
+      return;
+    }
+  });
+
+  // get node style
+  nodeArray.map((node) => {
+    console.log(node.style.transform);
+    nodePosition.push(node.style.transform);
+  });
+
+  // change node style to nodePosition[0] value
+  nodeArray.map((node) => {
+    node.style.transform = nodePosition[0];
+  });
+};
+
+function NodeIdDisplay() {
+  const nodeId = useNodeId();
+
+  return (
+    // <span className={styles.nodeId} onClick={foldUpCard}>
+    <span className={styles.nodeId}>{nodeId}</span>
+  );
+}
 
 const CustomNode: FC<NodeProps> = ({ data, isConnectable }) => {
   return (
@@ -11,6 +65,7 @@ const CustomNode: FC<NodeProps> = ({ data, isConnectable }) => {
       <Handle type="source" position={Position.Bottom} id="bottom" />
       <Handle type="target" position={Position.Top} id="top" />
       <div className={styles.card}>
+        <NodeIdDisplay />
         {data?.thumbnailPath && (
           <div className={styles.imageContainer}>
             <Image
